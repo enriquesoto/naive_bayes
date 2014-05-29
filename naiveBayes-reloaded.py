83f9bc1cd3af36655c5be868f7810ecaaf7c1bdb
enrique <enrique@enrique-HP-ENVY-15-Notebook-PC.(none)>
2014-05-29 18:56:33 -0500

    adding look up tables

--- naiveBayes-reloaded.py
+++ naiveBayes-reloaded.py
@@ -3,30 +3,31 @@ import collections
 import math
  
 class Model: 
-        def __init__(self, arffFile):
-                self.trainingFile = arffFile
+        def __init__(self, dataFile):
+                self.trainingFile = dataFile
                 self.features = {}      #all feature names and their possible values (including the class label)
                 self.featureNameList =[]      #this is to maintain the order of features as in the arff
                 self.featureCounts = collections.defaultdict(lambda: 1)#contains tuples of the form (label, feature_name, feature_value)
                 self.featureVectors = []        #contains all the values and the label as the last entry
                 self.labelCounts = collections.defaultdict(lambda: 0)   #these will be smoothed later
-                self.NumeroClasses =  {}
- 
+                self.numeroClasses =  {}
+                self.matrix = []
+
         def TrainClassifier(self):
                 #print "hello"
                 #print self.featureNameList
-                self.NumeroClases = len(self.features['class'])
+                self.numeroClases = len(self.features['class'])
                 #print self.features[self.featureNameList[0]]
-                Matrix = []
+                
                 for i in range(len(self.featureNameList)-1):
                         valoresPorAtributo = len(self.features[self.featureNameList[i]])
-                        Matrix.append ([[0 for x in xrange(self.NumeroClases)] for x in xrange(valoresPorAtributo)])  #columnas x fila
-                        
+                        self.matrix.append ([[0 for x in xrange(self.numeroClases)] for x in xrange(valoresPorAtributo)])  #columnas x fila
 
-                for row in self.featureVectors:
-                        #indexClass = len(row)-1
-                        print row
-
+                # for row in self.featureVectors:
+                #         #indexClass = len(row)-1
+                #         for item in row:
+                #                 print item 
+                
                                 
         def GetValues(self):
                 file = open(self.trainingFile, 'r')
@@ -48,9 +49,11 @@ class Model:
         #                         print "classifier: " + self.Classify(vector) + " given " + vector[len(vector) - 1]                                
 		
 if __name__ == "__main__":
-        model = Model("car.data")
+        model = Model("tennis.arff")
         model.GetValues()
         model.TrainClassifier()
-        
+        #print model.features['outlook'].index('rain')
+        #print model.featureNameList.index('temperature')
+        print model.matrix
         # model.TrainClassifier()
         # model.TestClassifier("tennis.arff")
